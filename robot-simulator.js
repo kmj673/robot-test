30f94f2c7ce11094ec774409d300b86c3d90f12d
Minju Kim <kmj673@gmail.com>
2022-03-20 19:14:44 +0000

    complete test code

--- robot-simulator.js
+++ robot-simulator.js
@@ -1,12 +1,50 @@
 class Robot {
-  constructor(x, y) {
-    this.xCoor = x;
-    this.yCoor = y;
+  constructor() {
+    this.xCoor = 0;
+    this.yCoor = 0;
+    this.face = "";
+    this.command = "";
   }
 
   execute(str) {
-    console.log(str);
+    let splitted = str.split(" ");
+    this.xCoor = parseInt(splitted[0]);
+    this.yCoor = parseInt(splitted[1]);
+    this.face = splitted[2];
+    this.command = splitted[3];
+    let commandArr = [...this.command];
+    for (let command of commandArr) {
+      if (command === "R") {
+        if (this.face === "NORTH") {
+          this.face = "EAST";
+        } else if (this.face === "EAST") {
+          this.face = "SOUTH";
+        } else if (this.face === "SOUTH") {
+          this.face = "WEST";
+        } else if (this.face === "WEST") {
+          this.face = "NORTH";
+        }
+      }
+      if (command === "L") {
+        if (this.face === "NORTH") {
+          this.face = "WEST";
+        } else if (this.face === "EAST") {
+          this.face = "NORTH";
+        } else if (this.face === "SOUTH") {
+          this.face = "EAST";
+        } else if (this.face === "WEST") {
+          this.face = "SOUTH";
+        }
+      }
+      if (command === "A") {
+        if (this.face === "NORTH") this.yCoor++;
+        if (this.face === "EAST") this.xCoor++;
+        if (this.face === "SOUTH") this.yCoor--;
+        if (this.face === "WEST") this.xCoor--;
+      }
+    }
+    return `${this.xCoor} ${this.yCoor} ${this.face}`;
   }
 }
 
-let robot = new Robot();
+module.exports = Robot;
